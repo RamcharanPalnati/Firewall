3ccd991b1112e8a790530fe6102d99b3d5b8b6fb
ramcharan.palnati <ramcharan.palnati@opsramp.com>
2019-12-21 23:48:40 +0530

    setSockOptions

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 
 #include <netdb.h>
 #include <netinet/in.h>
+
+#include <unistd.h> //unix close()
+
 #include <sys/socket.h>
 #include <sys/types.h>
 
@@ -20,6 +23,8 @@ public:
     void setPort(int port);
 
     int &getMasterSocket();
+
+    sockaddr_in *getSocketAddress();
 
     virtual bool listenServer(int port) = 0;
     virtual ~createServer();
@@ -68,29 +73,53 @@ int &createServer::getMasterSocket()
     return masterSocket;
 }
 
+sockaddr_in *createServer::getSocketAddress()
+{
+    return serverAddr;
+}
 
 void createServer::setPort(int port)
 {
-    this->serverAddr->sin_port=htons(port);
+    this->serverAddr->sin_port = htons(port);
 }
 
 bool createTcpServer ::listenServer(int port)
 {
     setPort(port);
     setMasterSocket(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP));
-    std::cout<<"TCP"<<getMasterSocket()<<std::endl;
-    if (getMasterSocket() < 0)
+
+    int &localMasterSocket = getMasterSocket();
+    int option = 1;
+    if (localMasterSocket < 0)
     {
         std::cout << "Unable to create TCP socket" << std::endl;
     }
+    if (setsockopt(localMasterSocket, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR), (char *)&option, sizeof(option)) < 0)
+    {
+        printf("setsockopt failed for REUSEPORT and REUSEADDR \n");
+        close(localMasterSocket);
+    }
+    if (setsockopt(localMasterSocket, SOL_SOCKET, SO_KEEPALIVE, (char *)&option, sizeof(option)) < 0)
+    {
+        printf("setsockopt failed for SO_KEEPALIVE \n");
+        close(localMasterSocket);
+    }
+    if (setsockopt(localMasterSocket, SOL_SOCKET, SO_BROADCAST, (char *)&option, sizeof(option)) < 0)
+    {
+        printf("setsockopt failed for SO_BROADCAST \n");
+        close(localMasterSocket);
+    }
+    
 }
 
 bool createUdpServer ::listenServer(int port)
 {
-    setPort(port);    
-    setMasterSocket(socket(AF_INET,SOCK_DGRAM, IPPROTO_UDP));
-    std::cout<<"UDP"<<getMasterSocket()<<std::endl;
-    if (getMasterSocket()< 0)
+    setPort(port);
+    setMasterSocket(socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP));
+    int option = 1;
+
+    int &localMasterSocket = getMasterSocket();
+    if (localMasterSocket < 0)
     {
         std::cout << "Unable to create UDP socket" << std::endl;
     }
@@ -99,8 +128,8 @@ bool createRawSocketServer ::listenServer(int port)
 {
     setPort(port);
     setMasterSocket(socket(AF_INET, SOCK_RAW, IPPROTO_ICMP));
-    std::cout<<"raw socket"<<getMasterSocket()<<std::endl;
-    if (getMasterSocket()< 0)
+    std::cout << "raw socket" << getMasterSocket() << std::endl;
+    if (getMasterSocket() < 0)
     {
         std::cout << "Unable to create RAW socket" << std::endl;
     }
@@ -110,11 +139,8 @@ int main(int argc, char **argv)
 {
     std::cout << "Main Firewall Started" << std::endl;
     createTcpServer *createTcp = new createTcpServer();
-    createTcp->listenServer(4566);
-    delete createTcp;    
-    createRawSocketServer *createRaw = new createRawSocketServer();
-    createRaw->listenServer(45436);
-    delete createRaw;
-
+    createTcp->listenServer(4566);    
+    delete createTcp;
+    
     return 0;
 }
